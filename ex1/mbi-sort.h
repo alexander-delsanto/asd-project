3888f6b5f98c4d4269cc2504db675f98d5fe9a70
st213994 <alexander.delsanto@edu.unito.it>
2023-04-04 17:23:53 +0200

    Added mbi-sort.h with first functions prototypes

---
+++ ex1/mbi-sort.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#ifndef _MBI_SORT_H
+#define _MBI_SORT_H
+#endif
+
+
+//void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void *));
+
+//void sort_records(const char *infile, const char *outfile, size_t k, size_t field);
+
+//void binary_insertion_sort(void *base, size_t nitems, size_t size);
+
+int binary_search(void *base, size_t size, int (*compar)(const void *, const void*), int left_index, int right_index, void *elem);
+
+int compare_int(const void *pointer1, const void *pointer2);
+
+int compare_double(const void *pointer1, const void *pointer2);
+
+// int compare_string(const void *pointer1, const void *pointer2);
+
