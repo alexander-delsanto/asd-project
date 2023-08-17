aca97e8fec8c160af0467269e447dc819e679b65
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-17 13:27:07 +0200

    adding comments

--- ex1/src/lib/include/mbi-sort.h
+++ ex1/src/lib/include/mbi-sort.h
@@ -4,9 +4,10 @@
 #define _MBI_SORT_H
 
 
-
+// Function that the user should call
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void*, const void*));
 
+// Functions used only for unit testing
 int binary_search(void *base, size_t size, int (*compar)(const void *, const void*), int left_index, int right_index, void *elem);
 
 void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
