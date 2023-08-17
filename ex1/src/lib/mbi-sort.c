b4a7f149ac8a600f5b72cf15224fa44dc261ea76
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-17 14:53:01 +0200

    change indentation from 8 size tab to 4 size space

--- ex1/src/lib/mbi-sort.c
+++ ex1/src/lib/mbi-sort.c
@@ -12,98 +12,98 @@ int binary_search(void *, size_t, int (*)(const void *, const void*), int, int, 
 
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void*, const void*))
 {
-	merge_sort(base, size, k, compar, 0, nitems - 1);
+    merge_sort(base, size, k, compar, 0, nitems - 1);
 }
 
 void merge_sort(void *base, size_t size, size_t k, int (*compar)(const void*, const void*), int l, int r)
 {
-	if(l < r){
-		size_t length = r - l + 1;
-		if(length <= k){
-			binary_insertion_sort(base + l * size, length, size, compar);
-			return;
-		}
-		int m = (l + r) / 2;
-		merge_sort(base, size, k, compar, l, m);
-		merge_sort(base, size, k, compar, m + 1, r);
-		merge(base, size, compar, l, m, r);
-	}
+    if(l < r){
+        size_t length = r - l + 1;
+        if(length <= k){
+            binary_insertion_sort(base + l * size, length, size, compar);
+            return;
+        }
+        int m = (l + r) / 2;
+        merge_sort(base, size, k, compar, l, m);
+        merge_sort(base, size, k, compar, m + 1, r);
+        merge(base, size, compar, l, m, r);
+    }
 }
 
 void merge(void *base, size_t size, int (*compar)(const void*, const void*), int l, int m, int r)
 {
-	int i, j, k;
-	int left_length = m - l + 1;
-	int right_length = r - m;
-	void *left_arr = malloc(left_length * size);
-	void *right_arr = malloc(right_length * size);
-	for(i = 0; i < left_length; i++){
-		memcpy(left_arr + (i * size), base + ((l + i) * size), size);
-	}
-	for(i = 0; i < right_length; i++){
-		memcpy(right_arr + (i * size), base + ((m + 1 + i) * size), size);
-	}
+    int i, j, k;
+    int left_length = m - l + 1;
+    int right_length = r - m;
+    void *left_arr = malloc(left_length * size);
+    void *right_arr = malloc(right_length * size);
+    for(i = 0; i < left_length; i++){
+        memcpy(left_arr + (i * size), base + ((l + i) * size), size);
+    }
+    for(i = 0; i < right_length; i++){
+        memcpy(right_arr + (i * size), base + ((m + 1 + i) * size), size);
+    }
 
-	i = 0;
-	j = 0;
-	k = l;
-	while(i < left_length && j < right_length){
-		if(compar(left_arr + i * size, right_arr + j * size) <= 0){
-			memcpy(base + k * size, left_arr + i * size, size);
-			i++;
-		}else{
-			memcpy(base + k * size, right_arr + j * size, size);
-			j++;
-		}
-		k++;
-	}
+    i = 0;
+    j = 0;
+    k = l;
+    while(i < left_length && j < right_length){
+        if(compar(left_arr + i * size, right_arr + j * size) <= 0){
+            memcpy(base + k * size, left_arr + i * size, size);
+            i++;
+        }else{
+            memcpy(base + k * size, right_arr + j * size, size);
+            j++;
+        }
+        k++;
+    }
 
-	while(i < left_length){
-		memcpy(base + k * size, left_arr + i * size, size);
-		i++;
-		k++;
-	}
+    while(i < left_length){
+        memcpy(base + k * size, left_arr + i * size, size);
+        i++;
+        k++;
+    }
 
-	while(j < right_length){
-		memcpy(base + k * size, right_arr + j * size, size);
-		j++;
-		k++;
-	}
+    while(j < right_length){
+        memcpy(base + k * size, right_arr + j * size, size);
+        j++;
+        k++;
+    }
 
-	free(left_arr);
-	free(right_arr);
-	return;
+    free(left_arr);
+    free(right_arr);
+    return;
 }
 
 void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 {
-	int i, j, k;
-	void *temp = malloc(size);
-	for (i = 1; (size_t) i < nitems; i++) {
-		j = binary_search(base, size, compar, 0, i-1, base + i * size);
-		k = i;
-		while (k > j) {
-			memcpy((void *) temp, base + (k - 1) * size, size);
-			memcpy(base + (k - 1) * size, base + k * size, size);
-			memcpy(base + k * size, (void * ) temp, size);
-			k--;
-		}
-	}
+    int i, j, k;
+    void *temp = malloc(size);
+    for (i = 1; (size_t) i < nitems; i++) {
+        j = binary_search(base, size, compar, 0, i-1, base + i * size);
+        k = i;
+        while (k > j) {
+            memcpy((void *) temp, base + (k - 1) * size, size);
+            memcpy(base + (k - 1) * size, base + k * size, size);
+            memcpy(base + k * size, (void * ) temp, size);
+            k--;
+        }
+    }
 }
 
 int binary_search(void *base, size_t size, int (*compar)(const void *, const void*), int left_index, int right_index, void *elem)
 {
-	if (right_index <= left_index) {
-		if(compar(elem, base + left_index * size) > 0)
-			return left_index + 1;
-		else return left_index;
-	} else {
-		int m = (left_index + right_index) / 2;
-		int compare_res = compar(elem, base + m * size);
-		if (compare_res == 0)
-			return m + 1;
-		if (compare_res > 0)
-			return binary_search(base, size, compar, m + 1, right_index, elem);
-		return binary_search(base, size, compar, left_index, m - 1, elem);
-	}
+    if (right_index <= left_index) {
+        if(compar(elem, base + left_index * size) > 0)
+            return left_index + 1;
+        else return left_index;
+    } else {
+        int m = (left_index + right_index) / 2;
+        int compare_res = compar(elem, base + m * size);
+        if (compare_res == 0)
+            return m + 1;
+        if (compare_res > 0)
+            return binary_search(base, size, compar, m + 1, right_index, elem);
+        return binary_search(base, size, compar, left_index, m - 1, elem);
+    }
 }
--- ex1/src/test/sort-test.c
+++ ex1/src/test/sort-test.c
@@ -4,11 +4,11 @@
 
 int compare_int(const void *int1_pointer, const void *int2_pointer)
 {
-	if(*(int *)int1_pointer == *(int *)int2_pointer)
-		return 0;
-	else if(*(int *)int1_pointer < *(int *)int2_pointer)
-		return -1;
-	return 1;
+    if(*(int *)int1_pointer == *(int *)int2_pointer)
+        return 0;
+    else if(*(int *)int1_pointer < *(int *)int2_pointer)
+        return -1;
+    return 1;
 }
 
 static int length = 6;
@@ -17,83 +17,83 @@ static const int ordered_int_arr[] = {-1, 0, 1, 2, 3, 4};
 
 void setUp(void)
 {
-	int_arr = malloc(sizeof(int) * length);
-	int_arr[0] = 4;
-	int_arr[1] = -1;
-	int_arr[2] = 2;
-	int_arr[3] = 0;
-	int_arr[4] = 1;
-	int_arr[5] = 3;
+    int_arr = malloc(sizeof(int) * length);
+    int_arr[0] = 4;
+    int_arr[1] = -1;
+    int_arr[2] = 2;
+    int_arr[3] = 0;
+    int_arr[4] = 1;
+    int_arr[5] = 3;
 }
 
 void tearDown(void)
 {
-	free(int_arr);
+    free(int_arr);
 }
 
 void test_insertion_sort_null_array(void)
 {
-	int *a = NULL;
-	binary_insertion_sort((void *) a, 0, 0, compare_int);
-	TEST_ASSERT_NULL(a);
+    int *a = NULL;
+    binary_insertion_sort((void *) a, 0, 0, compare_int);
+    TEST_ASSERT_NULL(a);
 }
 
 void test_insertion_sort_empty_array(void)
 {
-	int a[] = {};
-	binary_insertion_sort((void *) a, 0, 4, compare_int);
-	TEST_ASSERT_EQUAL_INT(0, sizeof(a));
+    int a[] = {};
+    binary_insertion_sort((void *) a, 0, 4, compare_int);
+    TEST_ASSERT_EQUAL_INT(0, sizeof(a));
 }
 
 void test_insertion_sort_one_element_array(void)
 {
-	int a[] = {1}, b[] = {1};
-	binary_insertion_sort((void *) a, 1, 4, compare_int);
-	TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    int a[] = {1}, b[] = {1};
+    binary_insertion_sort((void *) a, 1, 4, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
 }
 
 void test_insertion_sort_array(void)
 {
-	binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), compare_int);
-	TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, length);
+    binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, length);
 }
 
 void test_merge_sort_null_array(void)
 {
-	int *a = NULL;
-	merge_sort((void *) a, 0, 0, compare_int, 0, 0);
-	TEST_ASSERT_NULL(a);
+    int *a = NULL;
+    merge_sort((void *) a, 0, 0, compare_int, 0, 0);
+    TEST_ASSERT_NULL(a);
 }
 
 void test_merge_sort_one_element_array(void)
 {
-	int a[] = {1}, b[] = {1};
-	merge_sort((void *) a, 1, 0, compare_int, 0, 0);
-	TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    int a[] = {1}, b[] = {1};
+    merge_sort((void *) a, 1, 0, compare_int, 0, 0);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
 }
 
 void test_merge_sort(void)
 {
-	merge_sort((void *) int_arr, sizeof(int_arr[0]), 0, compare_int, 0, length - 1);
-	TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
+    merge_sort((void *) int_arr, sizeof(int_arr[0]), 0, compare_int, 0, length - 1);
+    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
 }
 
 void test_merge_binary_insertion_sort(void)
 {
-	merge_binary_insertion_sort((void *) int_arr, 6, sizeof(int_arr[0]), 3, compare_int);
-	TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
+    merge_binary_insertion_sort((void *) int_arr, 6, sizeof(int_arr[0]), 3, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
 }
 
 int main(void)
 {
-	UNITY_BEGIN();
-	RUN_TEST(test_insertion_sort_null_array);
-	RUN_TEST(test_insertion_sort_empty_array);
-	RUN_TEST(test_insertion_sort_one_element_array);
-	RUN_TEST(test_insertion_sort_array);
-	RUN_TEST(test_merge_sort_null_array);
-	RUN_TEST(test_merge_sort_one_element_array);
-	RUN_TEST(test_merge_sort);
-	RUN_TEST(test_merge_binary_insertion_sort);
-	UNITY_END();
+    UNITY_BEGIN();
+    RUN_TEST(test_insertion_sort_null_array);
+    RUN_TEST(test_insertion_sort_empty_array);
+    RUN_TEST(test_insertion_sort_one_element_array);
+    RUN_TEST(test_insertion_sort_array);
+    RUN_TEST(test_merge_sort_null_array);
+    RUN_TEST(test_merge_sort_one_element_array);
+    RUN_TEST(test_merge_sort);
+    RUN_TEST(test_merge_binary_insertion_sort);
+    UNITY_END();
 }
