716767afd3ad11fe015f7e253436057b47979e1c
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-18 18:56:27 +0200

    first implementation of sort_records

--- ex1/src/main.c
+++ ex1/src/main.c
@@ -7,6 +7,12 @@
 int compare_record_int_field(const void *, const void *);
 int compare_record_int_field(const void *, const void *);
 int compare_record_string_field(const void *, const void *);
+void print_record_to_file(FILE *, Record *, size_t);
+void free_record(Record *, size_t);
+size_t read_record(FILE *, Record **);
+void sort_records(FILE *, FILE *, size_t, size_t);
+
+
 
 
 // Data structure used to load csv file
@@ -58,7 +64,129 @@ int compare_record_string_field(const void *string1_p, const void *string2_p)
     return strcmp(rec1_p->string_field, rec2_p->string_field);
 }
 
-int main(/*int argc, char *argv[]*/)
+void print_record_to_file(FILE *outfile, Record *record, size_t n_records)
 {
+    for(int i = 0; i < n_records; i++)
+        fprintf(outfile, "%d,%s,%d,%f\n", record[i].index, record[i].string_field,
+            record[i].integer_field, record[i].double_field);
+}
+
+void free_record(Record *record, size_t n_records)
+{
+    for(size_t i = 0; i < n_records; i++)
+        free(record[i].string_field);
+    free(record);
+}
+
+size_t read_record(FILE *file, Record **file_record)
+{
+    char *current_read_line;
+    size_t buffer_size = 1024;
+    char buffer[buffer_size];
+    size_t n_records = 0;
+    size_t max_records = 2500000;
+
+    *file_record = malloc(sizeof(Record) * max_records);
+    if(*file_record == NULL){
+            dprintf(2, "main: unable to allocate memory\n");
+            exit(EXIT_FAILURE);
+        }
+    dprintf(1, "Loading records from file...\n");
+    while(fgets(buffer, buffer_size, file) != NULL){
+        current_read_line = malloc(buffer_size + 1);
+        if(current_read_line == NULL){
+            dprintf(2, "main: unable to allocate memory\n");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(current_read_line, buffer);
+        char *index_field_current_line = strtok(current_read_line, ",");
+        char *string_field_current_line = strtok(NULL, ",");
+        char *integer_field_current_line = strtok(NULL, ",");
+        char *double_field_current_line = strtok(NULL, ",");
+
+        if(n_records >= max_records){
+            max_records = max_records * 2;
+            *file_record = realloc(*file_record, sizeof(Record) * max_records);
+            if(*file_record == NULL){
+                dprintf(2, "main: unable to reallocate memory\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        char *string_field = malloc(strlen(string_field_current_line) + 1);
+        if(string_field == NULL){
+            dprintf(2, "main: unable to allocate memory\n");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(string_field, string_field_current_line);
+
+        (*file_record)[n_records].index = atoi(index_field_current_line);
+        (*file_record)[n_records].string_field = string_field;
+        (*file_record)[n_records].integer_field = atoi(integer_field_current_line);
+        (*file_record)[n_records].double_field = atof(double_field_current_line);
+
+        n_records++;
+        free(current_read_line);
+    }
+    return n_records;
+}
+
+void sort_records(FILE *infile, FILE *outfile, size_t k, size_t field)
+{
+    if(field < 1 || field > 3){
+        dprintf(2, "main: invalid <field> value\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t n_read_records;
+    Record *infile_record;
+    n_read_records = read_record(infile, &infile_record);
+
+    switch (field){
+        case 3:
+            dprintf(1, "Sorting records by double field...\n");
+            merge_binary_insertion_sort((void *) infile_record, n_read_records, sizeof(Record), k, compare_record_double_field);
+            break;
+        case 2:
+            dprintf(1, "Sorting records by integer field...\n");
+            merge_binary_insertion_sort((void *) infile_record, n_read_records, sizeof(Record), k, compare_record_int_field);
+            break;
+        default:
+            dprintf(1, "Sorting records by string field...\n");
+            merge_binary_insertion_sort((void *) infile_record, n_read_records, sizeof(Record), k, compare_record_string_field);
+            break;
+    }
+    dprintf(1, "Creating sorted records file...\n");
+    print_record_to_file(outfile, infile_record, n_read_records);
+
+    free_record(infile_record, n_read_records);
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 5){
+        dprintf(2, "Usage: main_ex1 <infile> <outfile> <k> <field>\n");
+        exit(EXIT_FAILURE);
+        }
+
+    // Setting up inputs for sort_records
+    size_t k, field;
+    FILE *infile, *outfile;
+
+    infile = fopen(argv[1], "r");
+    if(infile == NULL){
+        dprintf(2, "main: unable to open %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    outfile = fopen(argv[2], "w");
+    if(outfile == NULL){
+        dprintf(2, "main: unable to open %s\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+    k = atol(argv[3]);
+    field = atol(argv[4]);
+
+    sort_records(infile, outfile, k, field);
+
     exit(EXIT_SUCCESS);
 }
