188b42e75d098ab58fc21803b7b429e1e1aea272
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-19 16:45:29 +0200

    removing internal functions from header file

--- ex1/src/lib/include/mbi-sort.h
+++ ex1/src/lib/include/mbi-sort.h
@@ -1,17 +1,9 @@
-#include <stdlib.h>
-
 #ifndef _MBI_SORT_H
 #define _MBI_SORT_H
 
+#include <stdlib.h>
 
 // Function that the user should call
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void*, const void*));
-
-// Functions used only for unit testing
-int binary_search(void *base, size_t size, int (*compar)(const void *, const void*), int left_index, int right_index, void *elem);
-
-void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
-
-void merge_sort(void *base, size_t size, size_t k, int (*compar)(const void*, const void*), int l, int r);
 
 #endif
