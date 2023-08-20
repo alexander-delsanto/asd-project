76547d3b8caf5337950235987dc5a1ef3e974cba
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-20 19:19:15 +0200

    changed tests to only test public function

--- src/tests/test_ex1/sort_test.c
+++ src/tests/test_ex1/sort_test.c
@@ -30,69 +30,72 @@ void tearDown(void)
     free(int_arr);
 }
 
-void test_mbi_sort_null_array_k_zero(void)
+void test_mbi_sort_null_array(void)
 {
     int *a = NULL;
     merge_binary_insertion_sort((void *) a, 0, 0, 0, compare_int);
     TEST_ASSERT_NULL(a);
+    merge_binary_insertion_sort((void *) a, 0, 0, 3, compare_int);
+    TEST_ASSERT_NULL(a);
+    merge_binary_insertion_sort((void *) a, 0, 0, length, compare_int);
+    TEST_ASSERT_NULL(a);
+
 }
 
-void test_insertion_sort_empty_array(void)
+void test_mbi_sort_empty_array(void)
 {
     int a[] = {};
-    binary_insertion_sort((void *) a, 0, 4, compare_int);
+    merge_binary_insertion_sort((void *) a, 0, sizeof(int), 0, compare_int);
+    TEST_ASSERT_EQUAL_INT(0, sizeof(a));
+    merge_binary_insertion_sort((void *) a, 0, sizeof(int), 3, compare_int);
+    TEST_ASSERT_EQUAL_INT(0, sizeof(a));
+    merge_binary_insertion_sort((void *) a, 0, sizeof(int), length, compare_int);
     TEST_ASSERT_EQUAL_INT(0, sizeof(a));
 }
 
-void test_insertion_sort_one_element_array(void)
+void test_mbi_sort_one_element_array(void)
 {
     int a[] = {1}, b[] = {1};
-    binary_insertion_sort((void *) a, 1, 4, compare_int);
+    merge_binary_insertion_sort((void *) a, 1, sizeof(int), 0, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    merge_binary_insertion_sort((void *) a, 1, sizeof(int), 3, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    merge_binary_insertion_sort((void *) a, 1, sizeof(int), length, compare_int);
     TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
 }
 
-void test_insertion_sort_array(void)
+void test_mbi_sort_same_elements_array(void)
 {
-    binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), compare_int);
+    int a[] = {1, 1, 1, 1}, b[] = {1, 1, 1, 1};
+    merge_binary_insertion_sort((void *) a, 4, sizeof(int), 0, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    merge_binary_insertion_sort((void *) a, 4, sizeof(int), 3, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+    merge_binary_insertion_sort((void *) a, 4, sizeof(int), length, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
+}
+
+void test_mbi_sort_array(void)
+{
+    merge_binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), 0, compare_int);
     TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, length);
-}
-
-void test_merge_sort_null_array(void)
-{
-    int *a = NULL;
-    merge_sort((void *) a, 0, 0, compare_int, 0, 0);
-    TEST_ASSERT_NULL(a);
-}
-
-void test_merge_sort_one_element_array(void)
-{
-    int a[] = {1}, b[] = {1};
-    merge_sort((void *) a, 1, 0, compare_int, 0, 0);
-    TEST_ASSERT_EQUAL_INT_ARRAY(b, a, 1);
-}
-
-void test_merge_sort(void)
-{
-    merge_sort((void *) int_arr, sizeof(int_arr[0]), 0, compare_int, 0, length - 1);
-    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
-}
-
-void test_merge_binary_insertion_sort(void)
-{
-    merge_binary_insertion_sort((void *) int_arr, 6, sizeof(int_arr[0]), 3, compare_int);
-    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, 6);
+    tearDown();
+    setUp();
+    merge_binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), 3, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, length);
+    tearDown();
+    setUp();
+    merge_binary_insertion_sort((void *)int_arr, length, sizeof(int_arr[0]), length, compare_int);
+    TEST_ASSERT_EQUAL_INT_ARRAY(ordered_int_arr, int_arr, length);
 }
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_mbi_sort_null_array_k_zero);
-    RUN_TEST(test_insertion_sort_empty_array);
-    RUN_TEST(test_insertion_sort_one_element_array);
-    RUN_TEST(test_insertion_sort_array);
-    RUN_TEST(test_merge_sort_null_array);
-    RUN_TEST(test_merge_sort_one_element_array);
-    RUN_TEST(test_merge_sort);
-    RUN_TEST(test_merge_binary_insertion_sort);
+    RUN_TEST(test_mbi_sort_null_array);
+    RUN_TEST(test_mbi_sort_empty_array);
+    RUN_TEST(test_mbi_sort_one_element_array);
+    RUN_TEST(test_mbi_sort_same_elements_array);
+    RUN_TEST(test_mbi_sort_array);
     UNITY_END();
 }
