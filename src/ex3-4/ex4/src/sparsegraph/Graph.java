57a3c3715249b42e88071ba12b5398d015db387d
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-08-30 15:40:26 +0200

    creating Graph classh and implementing contructor

---
+++ src/ex3-4/ex4/src/sparsegraph/Graph.java
@@ -0,0 +1,17 @@
+package sparsegraph;
+
+import java.util.*;
+
+public class Graph<V,L> implements AbstractGraph<V,L> {
+
+    private final boolean directed;
+    private final boolean labelled;
+    HashMap<V, HashMap<V,L>> adjacencyMap;
+
+
+    public Graph(boolean directed, boolean labelled) {
+        this.directed = directed;
+        this.labelled = labelled;
+        adjacencyMap = new HashMap<>();
+    }
+}
