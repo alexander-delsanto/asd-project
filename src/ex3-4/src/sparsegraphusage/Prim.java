7714f5cf88adb9b097cf474c032ec3df208b451c
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-09-01 21:18:12 +0200

    first implementation of Prim.java

---
+++ src/ex3-4/src/sparsegraphusage/Prim.java
@@ -0,0 +1,83 @@
+package sparsegraphusage;
+
+import priorityqueue.PriorityQueue;
+import sparsegraph.*;
+
+import java.io.FileReader;
+import java.util.*;
+import java.io.BufferedReader;
+
+public class Prim {
+
+    @SuppressWarnings("unchecked")
+    public static <V, L extends Number> Collection<? extends AbstractEdge<V, L>> minimumSpanningForest(Graph<V, L> graph) {
+        LinkedList<Edge<V,L>> minimumSpanningForest = new LinkedList<>();
+        HashSet<V> visited = new HashSet<>();
+        HashSet<V> allVertices = new HashSet<>(graph.getNodes());
+        PriorityQueue<Edge<V,L>> priorityQueue = new PriorityQueue<>(Comparator.comparing(edge -> edge.getLabel().doubleValue()));
+        HashSet<Edge<V,L>> graphEdges = new HashSet<>((Collection<Edge<V, L>>) graph.getEdges());
+
+
+        while(visited.size() != allVertices.size()) {
+            LinkedList<Edge<V,L>> minimumSpanningTree = new LinkedList<>();
+            V start = allVertices.stream().filter(v -> !visited.contains(v)).findFirst().orElse(null);
+            if (start == null) break;
+            visited.add(start);
+
+            for (Edge<V,L> edge : graphEdges) {
+                if (edge.getStart().equals(start) || edge.getEnd().equals(start))
+                    priorityQueue.push(edge);
+            }
+
+            while (!priorityQueue.empty()) {
+                Edge<V,L> currentEdge = priorityQueue.top();
+                priorityQueue.pop();
+                V source = currentEdge.getStart();
+                V dest = currentEdge.getEnd();
+                if (visited.contains(source) && !visited.contains(dest)) {
+                    minimumSpanningTree.add(currentEdge);
+                    visited.add(dest);
+                    for (Edge<V,L> edge : graphEdges)
+                        if (edge.getStart().equals(dest) || edge.getEnd().equals(dest))
+                            priorityQueue.push(edge);
+                } else if (!visited.contains(source) && visited.contains(dest)) {
+                    minimumSpanningTree.add(currentEdge);
+                    visited.add(source);
+
+                    for (Edge<V,L> edge : graphEdges)
+                        if (edge.getStart().equals(source) || edge.getEnd().equals(source))
+                            priorityQueue.push(edge);
+                }
+            }
+            minimumSpanningForest.addAll(minimumSpanningTree);
+        }
+        return minimumSpanningForest;
+    }
+
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.err.println("Usage: java Prim <inputFile>");
+            System.exit(1);
+        }
+
+        Graph<String, Double> graph = new Graph<>(false, true);
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            while((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                graph.addNode(row[0]);
+                graph.addNode(row[1]);
+                graph.addEdge(row[0], row[1], Double.valueOf(row[2]));
+                System.out.println(row[0] + " to " + row[1] + " dist: " + Double.valueOf(row[2]));
+            }
+            graph.printEdges();
+            System.out.println(graph.numNodes());
+            System.out.println(graph.numEdges());
+            reader.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
