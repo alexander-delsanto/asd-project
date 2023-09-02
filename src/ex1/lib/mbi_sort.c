2ac67f03e036902910a14fbd0250f2cb10f707c8
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-09-02 18:41:15 +0200

    refactoring comments

--- src/ex1/lib/mbi_sort.c
+++ src/ex1/lib/mbi_sort.c
@@ -1,3 +1,8 @@
+/**
+ * @file mbi_sort.c
+ * @brief Merge-BinaryInsertion Sort implementation.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include "mbi_sort.h"
@@ -11,41 +16,36 @@ static int binary_search(void *, size_t, int (*)(const void *, const void*), int
 
 
 /**
- * Merge-BinaryInsertion Sort wrapper
+ * @brief Merge-BinaryInsertion Sort function.
  *
- * Wrapper function called by the user.
- * Calls the function that sorts a generic array using the Merge Sort and the BinaryInsertion Sort algorithms combined.
+ * Sorts a generic array by combining the Merge Sort and BinaryInsertion Sort algorithms. The function
+ * behaves like a standard Merge Sort when the length of the sub-array is greater than \( k \). However,
+ * when the length of the sub-array is less than or equal to \( k \), BinaryInsertion Sort is applied.
  *
  * @param[in,out] base pointer to the first element of the array.
  * @param[in] nitems number of elements in the array.
  * @param[in] size size of each element of the array.
- * @param[in] k when \p k >= array.length BinaryInsertion Sort is used; when \p k = 0 only Merge Sort is used.
- * @param[in] compar pointer to function used to determine how the array will be sorted.
- *
-*/
+ * @param[in] k threshold for switching to BinaryInsertion Sort. If \( k = 0 \), only Merge Sort is used.
+ * @param[in] compar function pointer used for element comparison.
+ */
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void*, const void*))
 {
     _merge_binary_insertion_sort(base, size, k, compar, 0, nitems - 1);
 }
 
 /**
- * Merge-BinaryInsertion Sort
+ * @brief Internal recursive function for Merge-BinaryInsertion Sort.
  *
- * Sorts a generic array using the Merge Sort and the BinaryInsertion Sort algorithms combined.\n
- * When \p k = 0 it behaves like a normal Merge Sort algorithm; in this case the array is sorted by recursively splitting the main
- * array into sub-arrays until one element arrays are reached; the merge function is then called to merge the sub-arrays back to the starting array,
- * while ordering them with the compar function.\n
- * When \p k > 0 the array is split until the sub-arrays length <= k; BinaryInsertion Sort is then called to sort the sub-arrays; merge is
- * then called to merge the sorted sub-arrays back to the starting array.
+ * Recursive function to sort and merge sub-arrays. It checks the length of the current sub-array against \( k \).
+ * If the length is less than or equal to \( k \), it uses BinaryInsertion Sort. Otherwise, it continues to split the sub-array.
  *
  * @param[in,out] base pointer to the first element of the array.
- * @param[in] size size of each element of the array.
- * @param[in] k when k >= array.length BinaryInsertion Sort is used; when k = 0 only Merge Sort is used.
- * @param[in] compar pointer to function used to determine how the array will be sorted.
+ * @param[in] size size of each element in the array.
+ * @param[in] k threshold for switching to BinaryInsertion Sort.
+ * @param[in] compar function pointer used for element comparison.
  * @param[in] l index of the leftmost element of the array or sub-array.
  * @param[in] r index of the rightmost element of the array or sub-array.
- *
-*/
+ */
 static void _merge_binary_insertion_sort(void *base, size_t size, size_t k, int (*compar)(const void*, const void*), int l, int r)
 {
     if(l < r){
@@ -62,19 +62,17 @@ static void _merge_binary_insertion_sort(void *base, size_t size, size_t k, int 
 }
 
 /**
- * Merge
+ * @brief Merge function for Merge Sort.
  *
- * Takes two sorted sub-arrays as input and merges them into the starting one in the right order, determined by the \p compar function.\n
- * The arrays are merged by comparing each element of the left and right array and copyig them in the right order back into the starting array.
+ * Merges two sorted sub-arrays into one sorted array.
  *
  * @param[in,out] base pointer to the first element of the array.
  * @param[in] size size of each element of the array.
- * @param[in] compar pointer to function used to determine how the array will be sorted.
+ * @param[in] compar function pointer used for element comparison.
  * @param[in] l index of the leftmost element of the array or sub-array.
  * @param[in] m index of the middle element of the array.
  * @param[in] r index of the rightmost element of the array or sub-array.
- *
-*/
+ */
 static void merge(void *base, size_t size, int (*compar)(const void*, const void*), int l, int m, int r)
 {
     int i, j, k;
@@ -120,19 +118,16 @@ static void merge(void *base, size_t size, int (*compar)(const void*, const void
 }
 
 /**
- * BinaryInsertion Sort
+ * @brief BinaryInsertion Sort algorithm.
  *
- * Sorts a generic array.\n
- * Starting from the second element of the array binary_search is called to determine the index where the element should be moved;
- * if the returned index is the same as the current one the next element is examined, otherwise it moves the element back k - j positions by
- * swapping it with the one that precedes it; because of this, the portion of the array from base to i is always sorted.
+ * A variation of the standard Insertion Sort. Instead of linear search, it uses binary search to place the current element
+ * at its correct position, which reduces the number of comparisons.
  *
  * @param[in,out] base pointer to the first element of the array.
  * @param[in] nitems number of elements in the array.
- * @param[in] size size of each element of the array.
- * @param[in] compar pointer to function used to determine how the array will be sorted.
- *
-*/
+ * @param[in] size size of each element in the array.
+ * @param[in] compar function pointer used for element comparison.
+ */
 static void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 {
     int i, j, k;
@@ -151,22 +146,18 @@ static void binary_insertion_sort(void *base, size_t nitems, size_t size, int (*
 }
 
 /**
- * BinarySearch
+ * @brief Binary search function.
  *
- * Finds the position that \p elem should be in for the array to be sorted.\n
- * The position is found by splitting the array into sub-arrays and comparing elem with the element at the middle index; if elem = array[m],
- * m + 1 is returned; if elem > array[m] the right sub-array is examined to find the position; else if elem < array[m], the left sub-array is examined;
- * when r <= l the function returns l + 1 if elem > array[l], else it returns l.
+ * Finds the correct position for a given element within a sorted array using binary search.
  *
  * @param[in] base pointer to the first element of the array.
- * @param[in] size size of each element of the array.
- * @param[in] compar pointer to function used to determine how the array will be sorted.
- * @param[in] l left index of the examined portion of the array.
- * @param[in] r right index of the examined portion of the array.
- * @param[in] elem element to be examined.
- *
- * @return The index where elem should be for the array to be sorted.
-*/
+ * @param[in] size size of each element in the array.
+ * @param[in] compar function pointer used for element comparison.
+ * @param[in] l left boundary of the search range.
+ * @param[in] r right boundary of the search range.
+ * @param[in] elem element for which the position needs to be found.
+ * @return the correct index for the given element.
+ */
 static int binary_search(void *base, size_t size, int (*compar)(const void *, const void*), int l, int r, void *elem)
 {
     if (r <= l) {
