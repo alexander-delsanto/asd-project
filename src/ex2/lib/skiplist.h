ce4c261b5c2939a4e5224653c15355a424f54d86
Alexander Delsanto <alexander.delsanto@edu.unito.it>
2023-09-02 18:31:49 +0200

    adding doxygen comments to header files

--- src/ex2/lib/skiplist.h
+++ src/ex2/lib/skiplist.h
@@ -1,28 +1,69 @@
+/**
+ * @file skiplist.h
+ * @brief Skiplist header file.
+ */
+
 #ifndef _SKIPLIST_H
 #define _SKIPLIST_H
 
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * @struct SkipList
+ *
+ * Represents a skiplist, which is a data structure that allows fast search within an ordered sequence of elements.
+ * It achieves this by maintaining a layered list with each layer having a higher level of sparsity.
+ */
 struct SkipList {
-    struct Node **heads;
-    size_t max_level;
-    size_t max_height;
-    int (*compare)(const void*, const void*);
+    struct Node **heads;                        /**< Pointers to the head nodes for each level in the skiplist. */
+    size_t max_level;                           /**< Current maximum level reached by a node of the skiplist; represents the highest populated level. */
+    size_t max_height;                          /**< Maximum potential number of levels a node in the skiplist can have. */
+    int (*compare)(const void*, const void*);   /**< Comparison function pointer for sorting the elements in the skiplist. */
 };
 
+/**
+ * @struct Node
+ *
+ * Represents a node in the skip list.
+ */
 struct Node {
-    struct Node **next;
-    size_t size;
-    void *item;
+    struct Node **next;                         /**< Array of pointers to the next nodes in each level, up to size. */
+    size_t size;                                /**< Height of the node. */
+    void *item;                                 /**< Generic pointer to the item the node holds. */
 };
 
+/**
+ * @brief Initialize a new skip list.
+ *
+ * @param[out] list a pointer to the skiplist pointer that needs to be initialized.
+ * @param[in] max_height the maximum height for the skiplist.
+ * @param[in] compar a function pointer to compare two items in the skiplist.
+ */
 void new_skiplist(struct SkipList **list, size_t max_height, int (*compar)(const void*, const void*));
 
+/**
+ * @brief Clear a skiplist and free the memory allocated to its elements.
+ *
+ * @param[in,out] list a pointer to the skip list pointer that needs to be cleared.
+ */
 void clear_skiplist(struct SkipList **list);
 
+/**
+ * @brief Insert an item into the skiplist.
+ *
+ * @param[in,out] list a pointer to the skiplist.
+ * @param[in] item the item to be inserted.
+ */
 void insert_skiplist(struct SkipList *list, void *item);
 
+/**
+ * @brief Search for an item in the skiplist.
+ *
+ * @param[in] list a pointer to the skiplist.
+ * @param[in] item the item to be searched for.
+ * @return a pointer to the item in the skiplist if found, NULL otherwise.
+ */
 const void* search_skiplist(struct SkipList *list, void *item);
 
 
--- src/ex2/lib/skiplist_utils.h
+++ src/ex2/lib/skiplist_utils.h
@@ -1,12 +1,37 @@
+/**
+ * @file skiplist_utils.h
+ * @brief Skiplist utilitiy functions header file.
+ */
+
+
 #ifndef _SKIPLIST_UTILS_H
 #define _SKIPLIST_UTILS_H
 
 #include "skiplist.h"
 
+/**
+ * @brief Check if a skiplist is empty.
+ *
+ * @param[in] list a pointer to the skiplist.
+ * @return returns 1 if the skiplist is empty, 0 otherwise.
+ */
 int skiplist_is_empty(struct SkipList *list);
 
+/**
+ * @brief Calculates the length (number of nodes) of a skiplist.
+ *
+ * @param[in] list a pointer to the skiplist.
+ * @return the length of the skiplist.
+ */
 size_t skiplist_length(struct SkipList *list);
 
+/**
+ * @brief Prints a skiplist of integer type items.
+ *
+ * @note This function assumes that the skiplist's item type is int.
+ *
+ * @param[in] list a pointer to the skiplist.
+ */
 void skiplist_print_int(struct SkipList *list);
 
 #endif
